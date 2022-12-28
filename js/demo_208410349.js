dc5f7f994ce5361364518b78cb772dd40f9b7b9a
JKYROC <JKYROC@gmail.com>
2022-12-28 11:59:44 +0800

    w13-P1:show fixed navbar

--- js/demo_208410349.js
+++ js/demo_208410349.js
@@ -1,20 +1,26 @@
-// alert("hi");
-function change(index){
+const navbar = document.querySelector('#nav');
+const date = document.querySelector('#date');
+// add fixed class to navbar
 
-const p = document.querySelector('.show-classdemo');
-    console.log('p' ,p);
-    switch(index){
-        case 1:
-        p.innerHTML = '<iframe src="./demo/w01-208410349/index.html" width=100% height=100%/>'
-        break;
-        case 2:
-        p.innerHTML = '<iframe src="./demo/w02-208410349/card.html" width=100% height=100%/>'
-        break;
-        case 3:
-        p.innerHTML = '<iframe src="./demo/w04-208410349/blog_208410349.html" width=100% height=100%/>'
-        break;
-        case 4:
-        p.innerHTML = '<iframe src="./demo/w06-208410349/blog_208410349.html" width=100% height=100%/>'
-        break;
-    }
-}
+const navBtn = document.querySelector('#nav-btn');
+const closeBtn = document.querySelector('#close-btn');
+const sidebar = document.querySelector('#sidebar');
+
+console.log('navBtn', navBtn);
+console.log('closeBtn', closeBtn);
+console.log('sidebar', sidebar);
+navBtn.addEventListener('click',function(){
+  sidebar.classList.add('show-sidebar');
+});
+closeBtn.addEventListener('click',function(){
+  sidebar.classList.remove('show-sidebar');
+});
+window.addEventListener('scroll', function () {
+  if (window.pageYOffset > 80) {
+    navbar.classList.add('navbar-fixed');
+  } else {
+    navbar.classList.remove('navbar-fixed');
+  }
+});
+// set year
+date.innerHTML = new Date().getFullYear();
